cfab77ac6b0c006be6acce4104bef172447021f9
Pavan <pavan234nallaa@gmail.com>
2025-09-21 16:54:31 +0530

    Fix MongoDB connection string and backend routes

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-
+require("dotenv").config();
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 // Connect to your ReactJourney database
-mongoose.connect('mongodb://127.0.0.1:27017/ReactJourney', {
+mongoose.connect(process.env.MAN, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
